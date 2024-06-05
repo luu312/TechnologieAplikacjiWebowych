3669994a3fd955344eff3a2fa833e75a97f30fab
Jacob <jchamielec303@gmail.com>
2024-06-05 18:45:57 +0200

    lab10

---
+++ blog/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { BlogComponent } from './components/blog/blog.component';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, BlogComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public title: string = 'blog';  
+  public counter: number = 0;
+
+  add() {
+    this.counter++;
+  }
+
+  remove() {
+    this.counter--;
+  }
+}
